c707156b6a479a9aa2a5ebada394881905ad4f5f
gcalder1 <george.calderon2000@gmail.com>
2025-05-19 20:16:19 -0400

    Added joke storage system that takes info from fetched joke and stores it into a personal array that we will print onto our table and search from

--- src/jokeTableLibrary.js
+++ src/jokeTableLibrary.js
@@ -1,6 +1,10 @@
 let personalJokeStorage = [];
 
-const saveJoke = (jokeData) => {
+/*
+We're storing the data from the fetched joke into our personal library array in the form of an object that
+has our necessary data
+*/
+export const saveJoke = (jokeData) => {
     const jokeStorageObject = {
         id: jokeData.id,
         category: jokeData.category,
@@ -11,23 +15,22 @@ const saveJoke = (jokeData) => {
         : jokeData.joke
     };
 
-    personalJokeStorage.push(jokeStorageObject);
-    updateJokeTable();
+    /*
+    We're checking for dupes and ensure jokes of those ID are not passed
+    */
+    if (!personalJokeStorage.some(joke => joke.id === jokeStorageObject.id)) {
+        personalJokeStorage.push(jokeStorageObject);
+        updateJokeTable();
+    };
     
 
-    // return jokeStorageObject
 };
 
-// const exampleJoke = {
-//     id: 69420,
-//     category: "Misc",
-//     type: "single",
-//     joke: "Spell PIG backwards."
-// }
-
-// console.log("This what the object looks like", saveJoke(exampleJoke));
-
-const updateJokeTable = () => {
+/*
+Populating the body of the table with the necessary data from our stored joke library by creating a row that
+will fit the individual cells of information
+*/
+export const updateJokeTable = () => {
     const tableBodyUpdate = document.getElementById("bodyJokes")
     tableBodyUpdate.innerHTML = '';
 
@@ -44,3 +47,29 @@ const updateJokeTable = () => {
     });
 };
 
+
+/*
+We're changing the rows we can actively see based on what category we search for
+*/
+export const searchJokesByCategory = category => {
+    const tableBody = document.getElementById("bodyJokes");
+    const tableRows = tableBody.getElementsByTagName("tr")
+
+    for (let row of tableRows) {
+        const categoryCell = row.cells[1].textContent.toLowerCase();
+        const searchByCategory = category.toLowerCase();
+        if (category === "") {
+            row.style.display = ""
+        } else if (categoryCell.includes(searchByCategory)) {
+            row.style.display = ""
+        } else {
+            row.style.display = "none"
+        }
+    }
+}
+
+/*
+We're creating a read-only replacement for our personalJokeStorage array using spread operator so that we
+can reference this instead of the OG array to avoid accidental changes to main array
+*/
+export const getAllJokes = () => [...personalJokeStorage];
